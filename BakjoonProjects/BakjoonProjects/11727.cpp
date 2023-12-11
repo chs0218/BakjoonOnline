635ddcb25354e02550ec83e16b85674fe67e0c58
chs0218 <39898936+chs0218@users.noreply.github.com>
2023-12-12 00:53:28 +0900

    11727 Solved

---
+++ BakjoonProjects/BakjoonProjects/11727.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int dp[1001];
+int main()
+{
+    int nTile;
+    cin >> nTile;
+
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 3;
+
+    for (int i = 3; i <= nTile; ++i)
+    {
+        dp[i] = dp[i - 1] + 2 * dp[i - 2];
+        dp[i] %= 10007;
+    }
+
+    cout << dp[nTile];
+}
