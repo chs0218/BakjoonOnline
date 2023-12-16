b1b3145daaa315c8ee6bb7052321ba44255e381a
chs0218 <39898936+chs0218@users.noreply.github.com>
2023-12-16 17:47:12 +0900

    11054 Solved

---
+++ BakjoonProjects/BakjoonProjects/11054.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int plusArr[1000];
+int minusArr[1000];
+int main()
+{
+    int arrNum;
+    cin >> arrNum;
+
+    vector<int> arr(arrNum);
+    for (int i = 0; i < arrNum; ++i)
+        cin >> arr[i];
+
+    for (int i = 0; i < arrNum; ++i)
+    {
+        plusArr[i] = 1;
+        for (int j = 0; j < i; ++j)
+        {
+            if (arr[j] < arr[i] && plusArr[i] < plusArr[j] + 1)
+                plusArr[i] = plusArr[j] + 1;
+        }
+    }
+
+    for (int i = arrNum - 1; i >= 0; --i)
+    {
+        minusArr[i] = 1;
+        for (int j = arrNum - 1; j >= 0; --j)
+        {
+            if (arr[j] < arr[i] && minusArr[i] < minusArr[j] + 1)
+                minusArr[i] = minusArr[j] + 1;
+        }
+    }
+
+    int ans = 0;
+    for (int i = 0; i < arrNum; ++i)
+        ans = max(ans, plusArr[i] + minusArr[i] - 1);
+    cout << ans;
+}
