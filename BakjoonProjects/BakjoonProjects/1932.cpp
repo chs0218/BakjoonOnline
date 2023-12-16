5ff3441916ccf8b796a13158db987d107bf6bba9
chs0218 <39898936+chs0218@users.noreply.github.com>
2023-12-16 18:45:18 +0900

    1932 Solved

---
+++ BakjoonProjects/BakjoonProjects/1932.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int dp[500][501];
+int main()
+{
+    int nSize = 0;
+    cin >> nSize;
+
+    for (int i = 0; i < nSize; ++i)
+    {
+        for (int j = 0; j < i + 1; ++j)
+            cin >> dp[i][j];
+    }
+
+    int ans = 0;
+    for (int i = 0; i < nSize; ++i)
+    {
+        for (int j = 0; j < i + 1; ++j)
+        {
+            if (j == 0) dp[i][j] = dp[i][j] + dp[i - 1][j];
+            else if (j == i) dp[i][j] = dp[i][j] + dp[i - 1][j - 1];
+            else {
+                dp[i][j] = max(dp[i][j] + dp[i - 1][j],
+                    dp[i][j] + dp[i - 1][j - 1]);
+            }
+
+            ans = max(ans, dp[i][j]);
+        }
+    }
+
+    cout << ans;
+}
