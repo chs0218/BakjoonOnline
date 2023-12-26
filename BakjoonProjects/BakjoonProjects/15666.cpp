bcafd9a25a485c8a53b6df4201c4ca81145ebf2d
chs0218 <39898936+chs0218@users.noreply.github.com>
2023-12-27 01:53:05 +0900

    15666 Solved

---
+++ BakjoonProjects/BakjoonProjects/15666.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+using namespace std;
+int N, M;
+int arr[8];
+int nSeq[8];
+void Seq(int nDepth, int nPrevIndex) {
+    if (nDepth == M) {
+        for (int i = 0; i < M; ++i)
+            cout << nSeq[i] << " ";
+        cout << "\n";
+        return;
+    }
+
+    int n = 0;
+    for (int i = nPrevIndex; i < N; ++i)
+    {
+        if (n != arr[i]) {
+            n = arr[i];
+            nSeq[nDepth] = arr[i];
+            Seq(nDepth + 1, i);
+        }
+    }
+}
+int main()
+{
+    cin >> N >> M;
+
+    for (int i = 0; i < N; ++i)
+        cin >> arr[i];
+
+    sort(arr, arr + N);
+
+    Seq(0, 0);
+}
