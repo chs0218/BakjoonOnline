ee3c845b8335266a92448c9aefbbc25280da1073
chs0218 <39898936+chs0218@users.noreply.github.com>
2023-12-27 01:28:29 +0900

    15652, 15654 Solved

---
+++ BakjoonProjects/BakjoonProjects/15652.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int N, M;
+int nSeq[8];
+void Seq(int nDepth, int nPrevious) {
+    if (nDepth == M) {
+        for (int i = 0; i < M; ++i)
+            cout << nSeq[i] << " ";
+        cout << "\n";
+        return;
+    }
+
+    for (int i = nPrevious; i <= N; ++i)
+    {
+        nSeq[nDepth] = i;
+        Seq(nDepth + 1, i);
+    }
+}
+int main()
+{
+    cin >> N >> M;
+    Seq(0, 1);
+}
