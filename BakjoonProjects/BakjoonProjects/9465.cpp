ce60e613f73d1e3de9554310f08d771bdf126ce2
chs0218 <39898936+chs0218@users.noreply.github.com>
2023-12-27 23:20:16 +0900

    9465 Solved

---
+++ BakjoonProjects/BakjoonProjects/9465.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <memory.h>
+using namespace std;
+int dp[3][100001];
+int score[3][100001];
+int main()
+{
+    int nTestCase;
+    cin >> nTestCase;
+
+    for (int k = 0; k < nTestCase; ++k)
+    {
+        memset(dp, 0, sizeof(dp));
+        int n;
+        cin >> n;
+
+        for (int i = 1; i <= 2; ++i)
+            for (int j = 1; j <= n; ++j)
+                cin >> score[i][j];
+
+        dp[1][1] = score[1][1];
+        dp[2][1] = score[2][1];
+
+        for (int i = 2; i <= n; ++i)
+        {
+            dp[1][i] = max(dp[2][i - 1] + score[1][i], dp[2][i - 2] + score[1][i]);
+            dp[2][i] = max(dp[1][i - 1] + score[2][i], dp[1][i - 2] + score[2][i]);
+        }
+
+        int ans = max(dp[1][n], dp[2][n]);
+        cout << ans << "\n";
+    }
+}
