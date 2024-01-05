127be2a402dd77f06399d0de00fb4400989f44d8
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-01-05 13:10:41 +0900

    1238 dijkstra Solved

---
+++ BakjoonProjects/BakjoonProjects/1238_dijkstra.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+struct Edge {
+    int v, w;
+};
+bool operator<(Edge lhs, Edge rhs)
+{
+    return lhs.w > rhs.w;
+}
+vector<Edge> vEdge[10001];
+int d[1001][1001];
+int N, M, X;
+void dijkstra(int n)
+{
+    for (int i = 0; i < N + 1; ++i)
+        d[n][i] = 1e9;
+
+    priority_queue<Edge> qEdge;
+    qEdge.push(Edge{ n, 0 });
+
+    while (!qEdge.empty())
+    {
+        int cur = qEdge.top().v;
+        int cost = qEdge.top().w;
+        qEdge.pop();
+
+        if (d[n][cur] < cost)
+            continue;
+
+        for (int i = 0; i < vEdge[cur].size(); ++i)
+        {
+            int nextNode = vEdge[cur][i].v;
+            int nextCost = vEdge[cur][i].w + cost;
+            if (d[n][nextNode] > nextCost)
+            {
+                d[n][nextNode] = nextCost;
+                qEdge.push(Edge{ nextNode, nextCost });
+            }
+        }
+    }
+}
+int main()
+{
+    cin >> N >> M >> X;
+
+    for (int i = 0; i < M; ++i)
+    {
+        int lhs, rhs, w;
+        cin >> lhs >> rhs >> w;
+        vEdge[lhs].push_back(Edge{ rhs, w });
+    }
+
+    for (int i = 1; i <= N; ++i)
+        dijkstra(i);
+
+    int ans = 0;
+    for (int i = 1; i <= N; ++i)
+        ans = max(ans, d[i][X] + d[X][i]);
+    cout << ans;
+}
