6dda899e81d90f5e1efbee77d276866fe528e63b
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-01-06 17:25:52 +0900

    1918 Solved

---
+++ BakjoonProjects/BakjoonProjects/1918.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+string ans;
+void changeStr(string str)
+{
+    stack<char> stChr;
+
+    for (int i = 0; i < str.size(); ++i)
+    {
+        if ('A' <= str[i] && str[i] <= 'Z')
+            ans.push_back(str[i]);
+        else {
+            if(str[i] == '(')
+                stChr.push(str[i]);
+            else if (str[i] == '*' || str[i] == '/')
+            {
+                while(!stChr.empty() && (stChr.top() == '*' || stChr.top() == '/'))
+                {
+                    ans.push_back(stChr.top());
+                    stChr.pop();
+                }
+                stChr.push(str[i]);
+            }
+            else if (str[i] == '+' || str[i] == '-')
+            {
+                while (!stChr.empty() && stChr.top() != '(')
+                {
+                    ans.push_back(stChr.top());
+                    stChr.pop();
+                }
+                stChr.push(str[i]);
+            }
+            else if (str[i] == ')') {
+                while (!stChr.empty() && stChr.top() != '(') {
+                    ans.push_back(stChr.top());
+                    stChr.pop();
+                }
+                stChr.pop();
+            }
+        }
+    }
+
+    while (!stChr.empty()) {
+        ans.push_back(stChr.top());
+        stChr.pop();
+    }
+}
+int main()
+{
+    string str;
+    cin >> str;
+
+    changeStr(str);
+
+    cout << ans;
+}
