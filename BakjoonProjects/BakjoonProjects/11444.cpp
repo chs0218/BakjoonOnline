0f3af4ad0aa9c76983dc6c70d5268b0ecb9e33f9
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-01-06 22:51:37 +0900

    Solved 11444 - Need Study

---
+++ BakjoonProjects/BakjoonProjects/11444.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+constexpr long long MOD = 1000000007;
+unordered_map<long long, long long> um;
+long long Fibo(long long n)
+{
+    if (um[n]) return um[n];
+
+    if (n % 2 == 0) um[n] = ((Fibo(n / 2) % MOD) * ((Fibo(n / 2 + 1) % MOD 
+        + Fibo(n / 2 - 1) % MOD) % MOD)) % MOD;
+    else um[n] = ((((Fibo((n + 1) / 2) % MOD) 
+        * (Fibo((n + 1) / 2) % MOD)) % MOD) 
+        + (((Fibo((n - 1) / 2) % MOD) * (Fibo((n - 1) / 2) % MOD)) % MOD)) % MOD;
+
+    return um[n];
+}
+int main()
+{
+    long long n;
+    cin >> n;
+
+    um[0] = 0;
+    um[1] = 1;
+    um[2] = 1;
+
+    cout << Fibo(n);
+}
