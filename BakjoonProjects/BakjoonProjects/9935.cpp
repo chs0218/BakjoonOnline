49092f1a3c2cf8850b45b9af8b653db13e39561c
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-01-08 00:48:40 +0900

    9935 Solved

---
+++ BakjoonProjects/BakjoonProjects/9935.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string strTotal, strBomb;
+    cin >> strTotal >> strBomb;
+
+    string ans;
+    for (int i = 0; i < strTotal.size(); ++i)
+    {
+        ans += strTotal[i];
+        if (ans.size() >= strBomb.size() && ans.back() == strBomb.back())
+        {
+            bool explode = true;
+            for (int j = 0; j < strBomb.size(); ++j)
+            {
+                char rhs = *(ans.rbegin() + j);
+                char lhs = *(strBomb.rbegin() + j);
+                if (rhs != lhs)
+                {
+                    explode = false;
+                    break;
+                }
+            }
+
+            if (explode) {
+                for (int j = 0; j < strBomb.size(); ++j)
+                    ans.erase(ans.end() - 1);
+            }
+        }
+    }
+
+    if (ans.empty()) cout << "FRULA";
+    else cout << ans;
+}
