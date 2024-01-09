da6758da7c40aacef1b244a5abc95811379f044f
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-01-09 18:04:24 +0900

    1562 Solved

---
+++ BakjoonProjects/BakjoonProjects/1562.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int N;
+constexpr long long MOD = 1000000000;
+long long stair[10][101][1024];
+int main()
+{
+	cin >> N;
+	for (int i = 1; i <= 9; ++i)
+		stair[i][1][1 << i] = 1;
+
+	for (int i = 2; i <= N; ++i)
+	{
+		for (int j = 0; j < 10; ++j)
+		{
+			for (int k = 0; k < 1024; ++k)
+			{
+				if (j == 0) {
+					stair[j][i][1 << j | k] += stair[j + 1][i - 1][k];
+					stair[j][i][1 << j | k] %= MOD;
+				}
+				else if (j == 9) {
+					stair[j][i][1 << j | k] += stair[j - 1][i - 1][k];
+					stair[j][i][1 << j | k] %= MOD;
+				}
+				else {
+					stair[j][i][1 << j | k] += stair[j - 1][i - 1][k];
+					stair[j][i][1 << j | k] += stair[j + 1][i - 1][k];
+					stair[j][i][1 << j | k] %= MOD;
+				}
+			}
+		}
+	}
+
+	long long ans = 0;
+	for (int i = 0; i < 10; ++i)
+	{
+		ans += stair[i][N][1023];
+		ans %= MOD;
+	}
+	cout << ans;
+}
