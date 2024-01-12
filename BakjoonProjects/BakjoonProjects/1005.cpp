9b675e2b57bab1618d1f0929139cd296916516e5
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-01-12 20:15:14 +0900

    1005 Solved

---
+++ BakjoonProjects/BakjoonProjects/1005.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int cost[1001];
+int ans[1001];
+int nTopology[1001];
+vector<int> vEdge[1001];
+int main()
+{
+    int T;
+    cin >> T;
+    while (T) {
+        int N, K;
+        cin >> N >> K;
+        for (int i = 1; i <= N; ++i)
+        {
+            cin >> cost[i];
+            nTopology[i] = 0;
+            ans[i] = cost[i];
+            vEdge[i].clear();
+        }
+
+        for (int i = 1; i <= K; ++i)
+        {
+            int lhs, rhs;
+            cin >> lhs >> rhs;
+            vEdge[lhs].push_back(rhs);
+            ++nTopology[rhs];
+        }
+
+        int goal;
+        cin >> goal;
+
+        queue<int> q;
+
+        for (int i = 1; i <= N; ++i)
+        {
+            if (nTopology[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+
+        while (!q.empty())
+        {
+            int cur = q.front();
+            q.pop();
+
+            for (int i = 0; i < vEdge[cur].size(); ++i)
+            {
+                int nextNode = vEdge[cur][i];
+                int nextCost = cost[nextNode] + ans[cur];
+
+                ans[nextNode] = max(ans[nextNode], nextCost);
+                if (--nTopology[nextNode] == 0)
+                    q.push(nextNode);
+            }
+        }
+
+        cout << ans[goal] << "\n";
+        --T;
+    }
+}
