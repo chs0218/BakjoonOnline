91ce8d0339515f447ba015b7bdd605e391a9dc76
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-01-18 21:03:31 +0900

    1766 Solved

---
+++ BakjoonProjects/BakjoonProjects/1766.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+
+    vector<int> vCount(N + 1);
+    vector<vector<int>> vEdge(N + 1);
+
+    for (int i = 0; i < M; ++i)
+    {
+        int lhs, rhs;
+        cin >> lhs >> rhs;
+
+        ++vCount[rhs];
+        vEdge[lhs].push_back(rhs);
+    }
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    for (int i = 1; i <= N; ++i)
+        if (vCount[i] == 0) pq.push(i);
+
+    while (!pq.empty())
+    {
+        int cur = pq.top();
+        pq.pop();
+
+        cout << cur << " ";
+
+        for (int i = 0; i < vEdge[cur].size(); ++i)
+        {
+            int next = vEdge[cur][i];
+            if (--vCount[next] == 0)
+                pq.push(next);
+        }
+    }
+}
