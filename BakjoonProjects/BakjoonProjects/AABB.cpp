1f16a4f698526e776d95526f5b76714e80146a4a
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-01-24 15:20:26 +0900

    Create AABB.cpp

---
+++ BakjoonProjects/BakjoonProjects/AABB.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+struct Rectangle {
+    int left;
+    int bottom;
+    int right;
+    int top;
+};
+
+bool CheckCollision(const Rectangle& rect1, const Rectangle& rect2) {
+    if (rect1.right < rect2.left) return false;
+    if (rect1.left < rect2.right) return false;
+    if (rect1.top < rect2.bottom) return false;
+    if (rect1.bottom < rect2.top) return false;
+    return true;
+}
+
+int main() {
+    Rectangle rectangle1 = { 0, 0, 3, 3 };
+    Rectangle rectangle2 = { 4, 4, 7, 7 };
+
+    if (CheckCollision(rectangle1, rectangle2)) {
+        std::cout << "Collide" << std::endl;
+    }
+    else {
+        std::cout << "Not Collide" << std::endl;
+    }
+    return 0;
+}
