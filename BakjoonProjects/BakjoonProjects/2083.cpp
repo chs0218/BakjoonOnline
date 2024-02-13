ebe4f9010cea7e0099c19bf0e76ff1d5b81ff42c
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-02-14 00:49:40 +0900

    2083 Solved

---
+++ BakjoonProjects/BakjoonProjects/2083.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    while (1) {
+        string name;
+        int age, weight;
+        cin >> name >> age >> weight;
+
+        if (name == "#") break;
+        if (age > 17 || weight >= 80)
+            cout << name << " Senior\n";
+        else
+            cout << name << " Junior\n";
+    }
+}
