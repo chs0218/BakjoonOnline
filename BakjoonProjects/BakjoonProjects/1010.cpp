be3aaad4239e5f18f45d389fd62f93f514d32ba3
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-03-20 17:11:13 +0900

    1010 C++ UnSolved
    DP 혹은 조합으로 풀어보기

---
+++ BakjoonProjects/BakjoonProjects/1010.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int T;
+    cin >> T;
+
+    for (int t = 0; t < T; ++t)
+    {
+        int N, M;
+        cin >> N >> M;
+
+        // M개 중에 N개를 선택
+
+
+    }
+}
