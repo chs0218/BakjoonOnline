17d778291d61c13d4af291752c9cb6d0d924b1a2
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-05-23 16:33:53 +0900

    2660 Solved

---
+++ BakjoonProjects/BakjoonProjects/2660.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int vScore[51][51];
+int main()
+{
+    for (int i = 1; i <= 50; ++i)
+        for (int j = 1; j <= 50; ++j)
+            (i == j) ? vScore[i][j] = 0 : vScore[i][j] = 1e9;
+
+    int N;
+    cin >> N;
+    while (1)
+    {
+        int l, r;
+        cin >> l >> r;
+
+        if (l == -1) break;
+
+        vScore[l][r] = 1;
+        vScore[r][l] = 1;
+    }
+
+    for (int k = 1; k <= N; ++k)
+    {
+        for (int i = 1; i <= N; ++i)
+        {
+            for (int j = 1; j <= N; ++j)
+            {
+                vScore[i][j] = min(vScore[i][j], vScore[i][k] + vScore[k][j]);
+            }
+        }
+    }
+
+    int ansScore = 1e9;
+    vector<int> vAns;
+
+    for (int i = 1; i <= N; ++i)
+    {
+        int score = 0;
+        for (int j = 1; j <= N; ++j)
+        {
+            if (score < vScore[i][j])
+                score = vScore[i][j];
+        }
+
+        if (score < ansScore)
+        {
+            ansScore = score;
+            vAns.clear();
+        }
+
+        if (score == ansScore)
+        {
+            vAns.push_back(i);
+        }
+    }
+
+    cout << ansScore << " " << vAns.size() << "\n";
+    for (const int& ans : vAns)
+        cout << ans << " ";
+}
