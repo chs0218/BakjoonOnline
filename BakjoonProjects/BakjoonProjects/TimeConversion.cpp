a44af0f013e1f9b0f85f22c1a68e59f927881eb5
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-10-19 22:14:59 +0900

    Time Conversion With regex

--- BakjoonProjects/BakjoonProjects/TimeConversion.cpp
+++ BakjoonProjects/BakjoonProjects/TimeConversion.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
+#include <regex>
 #include <string>
 using namespace std;
-int main()
-{
-    string s;
-    cin >> s;
 
+string timeConversion1(string s) {
     string hourStr = string{ s.begin(), s.begin() + 2 };
     string leftStr = string(s.begin() + 2, s.end() - 2);
     string timeFormat = string{ s.end() - 2, s.end() };
@@ -13,5 +11,35 @@ int main()
     int nHour = stoi(hourStr);
     if (timeFormat == "AM" && nHour == 12) nHour -= 12;
     if (timeFormat == "PM" && nHour != 12) nHour += 12;
-    cout << nHour << leftStr;
+
+    string result = to_string(nHour) + leftStr;
+    if (nHour < 10) result = "0" + result;
+    return result;
+}
+
+string timeConversion2(string s) {
+    regex timePattern(R"((\d{2}):(\d{2}):(\d{2})(AM|PM))");
+    smatch match;
+
+    bool bMatch = regex_match(s, match, timePattern);
+    if (false == bMatch)
+    {
+        return string{};
+    }
+
+    int nHour = stoi(match[0]);
+    if (match[4] == "AM" && nHour == 12) nHour -= 12;
+    if (match[4] == "PM" && nHour != 12) nHour += 12;
+
+    string result = to_string(nHour) + ":" + match[2].str() + ":" + match[3].str();
+    if (nHour < 10) result = "0" + result;
+    return result;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    cout << timeConversion2(s);
 }
