df9c019cf50b008c54765631d63343e9d7cf2190
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-10-20 23:42:47 +0900

    1060 Solving

---
+++ BakjoonProjects/BakjoonProjects/1060.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+
+    int L;
+    cin >> L;
+
+    vector<int> S(L);
+    for (int i = 0; i < L; ++i)
+        cin >> S[i];
+
+
+}
